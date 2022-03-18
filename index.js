0271d2c3d67e6ad863d9128232a6e6853af1e961
Rashed Altarmoom <rashed.al6armoom@gmail.com>
2022-03-18 22:39:01 +0300

    UPDATE finishing challenge 2 and 3

--- index.js
+++ index.js
@@ -11,4 +11,31 @@ function calculateBMI() {
   height = height / 100;
   let BMI = weight / height ** 2;
   alert(`Your BMI is ${BMI}`);
+  if (BMI < 18.5) {
+    alert("YOUR UNDERWEIGHT");
+  } else if (BMI >= 18.5 && BMI <= 24.9) {
+    alert("YOUR HEALTHY WEIGHT");
+  } else if (BMI >= 25.0 && BMI <= 29.9) {
+    alert("YOUR OVERWEIGHT");
+  } else {
+    alert("YOUR OBESE");
+  }
+
+  if (age < 19) {
+    alert("Your age is not included in the table");
+  } else if (age >= 19 && age <= 24 && BMI >= 19 && BMI <= 24) {
+    alert("YOUR BMI NORMAL WEIGHT BASED ON YOUR AGE");
+  } else if (age >= 25 && age <= 34 && BMI >= 20 && BMI <= 25) {
+    alert("YOUR BMI NORMAL WEIGHT BASED ON YOUR AGE");
+  } else if (age >= 35 && age <= 44 && BMI >= 21 && BMI <= 26) {
+    alert("YOUR BMI NORMAL WEIGHT BASED ON YOUR AGE");
+  } else if (age >= 45 && age <= 54 && BMI >= 22 && BMI <= 27) {
+    alert("YOUR BMI NORMAL WEIGHT BASED ON YOUR AGE");
+  } else if (age >= 55 && age <= 64 && BMI >= 23 && BMI <= 28) {
+    alert("YOUR BMI NORMAL WEIGHT BASED ON YOUR AGE");
+  } else if (age >= 65 && BMI >= 24 && BMI <= 29) {
+    alert("YOUR BMI NORMAL WEIGHT BASED ON YOUR AGE");
+  } else {
+    alert("YOUR BMI WEIGHT IS NOT NORMAL BASED ON YOUR AGE");
+  }
 }
